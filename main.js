6c63276dd52f024b6e45ab6d4fb98e4a1875e4f3
Catherinero <88639274+Catherinero@users.noreply.github.com>
2022-03-08 21:04:34 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+	farbic.image.fromURL('BirthdayImage.jpg', function(Img) {}
+    block_image_object = Img;
+    
+    block_image_objects.scaleToWidth(700);  
+    block_image_objects.scaleToHeight(510);
+    block_image_objects.set(
+    top:0
+    });
+    canvas.add(block_image_object);
+    });
+}
+
+function playSound(){
+x.play();	
+}
